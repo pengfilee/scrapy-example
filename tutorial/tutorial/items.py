5d53adbc769807768743e71eb5e6484cc47f3a40
lipengfei <lipengfei@megvii.com>
2018-10-30 18:57:56 +0800

    add

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -9,7 +9,5 @@ import scrapy
 
 
 class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    title = scrapy.Field()
-    link= scrapy.Field()
-    desc= scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
--- tutorial/tutorial/spiders/demz_spider.py
+++ tutorial/tutorial/spiders/demz_spider.py
@@ -3,15 +3,13 @@ from tutorial.items import TutorialItem
 class DemzSpider(scrapy.Spider):
     name='test'
     start_urls = [
-        "https://scrapy-chs.readthedocs.io/zh_CN/0.24/intro/tutorial.html"
+        "http://news.163.com/16/1107/15/C59GQULA00014AEE.html"
     ]
 
     def parse(self,response):
-        for sel in response.xpath('//ul/li'):
+        for sel in response.xpath('//p/span'):
             item=TutorialItem()
-            item['title'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
+            item['image_urls'] =['http://cms-bucket.nosdn.127.net/catchpic/5/54/54841f0d54ec20d30f315aaaa8a8585e.png'] 
             yield item
 
 
